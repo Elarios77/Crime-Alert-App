857c032215a0911bacfc3bd41d8f5402ee83a8c0
lazker <lazker@arx.net>
2026-03-27 17:53:26 +0200

    create HomeScreen

--- app/src/main/java/com/example/criminalalertapp/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/criminalalertapp/ui/home/screen/HomeScreen.kt
@@ -2,14 +2,26 @@ package com.example.criminalalertapp.ui.home.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardDefaults.cardElevation
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,20 +31,21 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.criminalalertapp.R
-import com.example.criminalalertapp.ui.report.screen.reportScreenCardItemList
 import com.example.criminalalertapp.ui.theme.CriminalAlertAppTheme
 
 @Composable
 fun HomeScreen() {
     Column(
         modifier = Modifier
-            .systemBarsPadding()
             .fillMaxSize()
+            .systemBarsPadding()
             .background(
                 brush = Brush.verticalGradient(
                     colorStops = arrayOf(
@@ -62,20 +75,90 @@ fun HomeScreen() {
                 Text(
                     text = stringResource(R.string.crime_alert),
                     color = colorResource(R.color.PoliceDarkBlue),
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 25.sp
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.SemiBold
                 )
                 Text(
                     text = stringResource(R.string.network),
                     color = colorResource(R.color.PoliceDarkBlue),
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 25.sp
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.SemiBold
                 )
                 Text(
                     text = stringResource(R.string.hs_subtitle),
                     color = colorResource(R.color.PoliceDarkBlue),
-                    fontWeight = FontWeight.Normal,
-                    fontSize = 18.sp
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Normal
+                )
+            }
+        }
+        HorizontalDivider(color = Color.White)
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(16.dp),
+            contentPadding = PaddingValues(bottom = 100.dp)
+        ) {
+            items(items = homeScreenCardItemList, itemContent = { item ->
+                Card(
+                    modifier = Modifier
+                        .padding(bottom = 12.dp)
+                        .height(120.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    colors = CardDefaults.cardColors(Color.White),
+                    elevation = cardElevation(10.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Image(
+                            painter = painterResource(item.image),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(100.dp)
+                                .padding(24.dp)
+
+                        )
+                        Column(
+                            modifier = Modifier.padding(8.dp),
+                            horizontalAlignment = Alignment.Start,
+                            verticalArrangement = Arrangement.spacedBy(4.dp)
+                        ) {
+                            Text(
+                                text = stringResource(item.text),
+                                color = Color.Black,
+                                fontSize = 18.sp,
+                                fontWeight = FontWeight.W500
+                            )
+                            Text(
+                                text = stringResource(item.subtext),
+                                color = Color.Black,
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight.W400
+                            )
+                        }
+                    }
+                }
+            }
+            )
+            item {
+                Text(
+                    text = stringResource(R.string.hs_message),
+                    modifier = Modifier.padding(top = 8.dp),
+                    color = Color.Black,
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = stringResource(R.string.hs_slogan),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    color = Color.Blue.copy(alpha = 0.8f),
+                    fontSize = 25.sp,
+                    textAlign = TextAlign.Center,
+                    fontStyle = FontStyle.Italic
                 )
             }
         }
--- app/src/main/java/com/example/criminalalertapp/ui/home/screen/HomeScreenCardItem.kt
+++ app/src/main/java/com/example/criminalalertapp/ui/home/screen/HomeScreenCardItem.kt
@@ -1,26 +1,25 @@
-package com.example.criminalalertapp.ui.report.screen
+package com.example.criminalalertapp.ui.home.screen
 
-import androidx.compose.ui.res.stringResource
 import com.example.criminalalertapp.R
 
-data class ReportScreenCardItem(
+data class HomeScreenCardItem(
     val image: Int,
     val text: Int,
     val subtext: Int
 )
 
-val reportScreenCardItemList = listOf(
-    ReportScreenCardItem(
+val homeScreenCardItemList = listOf(
+    HomeScreenCardItem(
         image = R.drawable.alert_bell,
         text = R.string.hs_cardItem1,
         subtext = R.string.hs_cardItem1_text
     ),
-    ReportScreenCardItem(
+    HomeScreenCardItem(
         image = R.drawable.map,
         text = R.string.hs_cardItem2,
         subtext = R.string.hs_cardItem2_text
     ),
-    ReportScreenCardItem(
+    HomeScreenCardItem(
         image = R.drawable.handshake,
         text = R.string.hs_cardItem3,
         subtext = R.string.hs_cardItem3_title
